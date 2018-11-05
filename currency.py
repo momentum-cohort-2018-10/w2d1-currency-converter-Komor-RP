c49af2794e35bcd5e4665ef866e51d1b49d2f0dd
Fan Huang <fan.huang.33@gmail.com>
2018-11-05 16:06:24 -0500

    added indirect conversion

--- currency.py
+++ currency.py
@@ -2,16 +2,29 @@
 def convert(rates, value, from_currency, to_currency):
     if from_currency == to_currency:
         return value
-
+    
+        
+    available_rates = []
     for conversion in rates:
-        if (from_currency and to_currency) in conversion:
+
+        available_rates.append(conversion[0])
+        available_rates.append(conversion[1])
+        
+        if (from_currency in conversion) and (to_currency in conversion):
             if from_currency == conversion[0]:
                 return value * conversion[2]
             elif from_currency == conversion[1]:
                 return value / conversion[2]
     
+    for conversion in rates:
+        if (from_currency in conversion) and (to_currency not in conversion) and (to_currency in available_rates):
+            if from_currency == conversion[0]:
+                return convert(rates, value * conversion[2], conversion[1], to_currency)
+            elif from_currency == conversion[1]:
+                return convert(rates, value / conversion[2], conversion[0], to_currency)
+                        
+    
     raise ValueError("Conversion rates unknown.")
-        
 
     
 
--- test_currency.py
+++ test_currency.py
@@ -29,5 +29,11 @@ def test_jpy_to_euro():
     assert convert(rates, 1, "JPY", "EUR") == 1 / 145.949 
 
 def test_raise_error_for_unknown_rates():
-    with pytest.raises(ValueError, message="Conversion rates unknown."):
-        convert(rates, 1, "JPY", "USD") 
+    with pytest.raises(ValueError, message = "Conversion rates unknown."):
+        convert(rates, 1, "JPY", "CAD") 
+
+def test_indirect_conversion_usd_to_jpy():
+    assert convert(rates, 1, "USD", "JPY") == pytest.approx(.74 * 145.949, .1)
+
+def test_indirect_conversion_jpy_to_usd():
+    assert convert(rates, 1, "JPY", "USD") == pytest.approx( (1 / 145.949) / .74 , .1)
