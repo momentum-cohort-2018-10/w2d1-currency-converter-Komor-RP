ffb06f510451c3ef4a84a76f5e4a23d7a3f2b6ee
Fan Huang <fan.huang.33@gmail.com>
2018-11-05 13:38:45 -0500

    one dollar converts to .74 euros

--- currency.py
+++ currency.py
@@ -1,5 +1,15 @@
 
 def convert(rates, value, from_currency, to_currency):
-    return 1
+    if from_currency == to_currency:
+        return value
+        
+    for conversion in rates:
+        if (from_currency and to_currency) in conversion:
+            if from_currency == conversion[0]:
+                return value * conversion[2]
+        
+
+    
+
 
 
--- test_currency.py
+++ test_currency.py
@@ -1,6 +1,9 @@
 from currency import *
 
-rates = []
+rates = [("USD", "EUR", 0.74)]
 
 def test_converts_to_itself():
     assert convert(rates, 1, "USD", "USD") == 1
+
+def test_one_dollar_to_euro():
+    assert convert(rates, 1, "USD", "EUR") == 0.74
