0407aab6a63fcc39620a35a8019dc96195aadf7e
Fan Huang <fan.huang.33@gmail.com>
2018-11-05 14:32:21 -0500

    raise ValueError for unknown rates

--- currency.py
+++ currency.py
@@ -9,6 +9,8 @@ def convert(rates, value, from_currency, to_currency):
                 return value * conversion[2]
             elif from_currency == conversion[1]:
                 return value / conversion[2]
+    
+    raise ValueError("Conversion rates unknown.")
         
 
     
--- test_currency.py
+++ test_currency.py
@@ -1,7 +1,7 @@
 from currency import *
 import pytest
 
-rates = [("USD", "EUR", 0.74)]
+rates = [("USD", "EUR", 0.74), ("EUR", "JPY", 145.949)]
 
 def test_converts_to_itself():
     assert convert(rates, 1, "USD", "USD") == 1
@@ -14,3 +14,20 @@ def test_convert_with_value():
 
 def test_one_euro_to_dollar():
     assert convert(rates, 1, "EUR", "USD") == pytest.approx(1.35, 0.1)
+
+def test_usd_to_euro(): 
+    assert convert(rates, 3, "USD", "EUR") == pytest.approx(2.22, .1)
+
+def test_euro_to_usd():
+    assert convert(rates, 2, "EUR", "USD") == pytest.approx(2.7, .1)
+
+def test_euro_to_jpy():
+    assert convert(rates, 1, "EUR", "JPY") == 145.949
+    assert convert(rates, 2, "EUR", "JPY") == 145.949 * 2
+
+def test_jpy_to_euro():
+    assert convert(rates, 1, "JPY", "EUR") == 1 / 145.949 
+
+def test_raise_error_for_unknown_rates():
+    with pytest.raises(ValueError, message="Conversion rates unknown."):
+        convert(rates, 1, "JPY", "USD") 
